5bf89acb62e222f353580b87ad3be6a9ff703f49
DGutierrez81 <DGutierrezbueno81@gmail.com>
2023-11-02 14:21:20 +0100

    instalando dependencias

---
+++ app/src/main/java/Ejercicios/jetpack/MainActivity.kt
@@ -0,0 +1,47 @@
+package Ejercicios.jetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import Ejercicios.jetpack.ui.theme.JetpackTheme
+import androidx.constraintlayout.compose.ConstraintLayout
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackTheme {
+        Greeting("Android")
+    }
+}
